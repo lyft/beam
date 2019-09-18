b3675c3df609ef4c07e0bf9e6deebbafebd44fd7
Thomas Weise <tweise@beamperfjobmanager-production-iad-052248f6e4be17ba3.lyft.net>
2019-09-18 21:00:13 +0000

    Commit for release 2.14.0-lyft201909181

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -314,7 +314,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.14.0'
+    project.version = '2.14.0-lyft201909181'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
