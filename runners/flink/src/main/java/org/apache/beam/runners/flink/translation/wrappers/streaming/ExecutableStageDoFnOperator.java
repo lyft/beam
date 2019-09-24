0caa909e13267940f1946d1049f049031c4bee89
Maximilian Michels <mxm@apache.org>
2019-09-24 15:11:53 +0200

    [BEAM-6733] Use Flink's prepareSnapshotPreBarrier to replace BufferedOutputManager
    For Flink version <= 1.5 the Flink Runner had to buffer any elements which are
    emitted during a snapshot because the barrier has already been emitted.
    
    Flink version >= 1.6 provides a hook to execute an action before the snapshot
    barrier is emitted by the operator. We can remove the buffering in favor of
    finishing the current bundle in the DoFnOperator's prepareSnapshotPreBarrier.
    
    This had previously been deferred (#7940) until removal of Flink 1.5 (#9632).

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
@@ -177,11 +177,6 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
   }
 
   @Override
-  protected Lock getLockToAcquireForStateAccessDuringBundles() {
-    return stateBackendLock;
-  }
-
-  @Override
   public void open() throws Exception {
     executableStage = ExecutableStage.fromPayload(payload);
     initializeUserState(executableStage, getKeyedStateBackend());
@@ -564,7 +559,7 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
     private final StageBundleFactory stageBundleFactory;
     private final StateRequestHandler stateRequestHandler;
     private final BundleProgressHandler progressHandler;
-    private final BufferedOutputManager<OutputT> outputManager;
+    private final FlinkOutputManager<OutputT> outputManager;
     private final Map<String, TupleTag<?>> outputMap;
     /** Timer Output Pcollection id => TimerSpec. */
     private final Map<String, TimerSpec> timerOutputIdToSpecMap;
@@ -581,7 +576,7 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
         StageBundleFactory stageBundleFactory,
         StateRequestHandler stateRequestHandler,
         BundleProgressHandler progressHandler,
-        BufferedOutputManager<OutputT> outputManager,
+        FlinkOutputManager<OutputT> outputManager,
         Map<String, TupleTag<?>> outputMap,
         Coder<BoundedWindow> windowCoder,
         BiConsumer<WindowedValue<InputT>, TimerInternals.TimerData> timerRegistration,
