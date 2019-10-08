442f6b5d18ebea50529acf60253c23c19b4f0755
Maximilian Michels <mxm@apache.org>
2019-10-08 17:50:32 +0200

    [BEAM-8383] Add metrics to the Python state cache
    For more insight into how effectively the cache works, metrics have been added
    to the Python SDK. All the state operations are counted, as well as metrics like
    the current size of the cache, cache hits/misses, and the capacity of the cache.
    
    [BEAM-8383] Refinements to the metrics
    
    - gauge per bundle
    - additional counters across bundles
    - metric isolation between workers

--- sdks/python/apache_beam/runners/portability/flink_runner_test.py
+++ sdks/python/apache_beam/runners/portability/flink_runner_test.py
@@ -30,9 +30,12 @@ from tempfile import mkdtemp
 import apache_beam as beam
 from apache_beam import Impulse
 from apache_beam import Map
+from apache_beam import Pipeline
+from apache_beam.coders import VarIntCoder
 from apache_beam.io.external.generate_sequence import GenerateSequence
 from apache_beam.io.external.kafka import ReadFromKafka
 from apache_beam.io.external.kafka import WriteToKafka
+from apache_beam.io.flink.flink_streaming_impulse_source import FlinkStreamingImpulseSource
 from apache_beam.metrics import Metrics
 from apache_beam.options.pipeline_options import DebugOptions
 from apache_beam.options.pipeline_options import PortableOptions
@@ -41,6 +44,7 @@ from apache_beam.runners.portability import portable_runner
 from apache_beam.runners.portability import portable_runner_test
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
+from apache_beam.transforms import userstate
 
 if __name__ == '__main__':
   # Run as
@@ -106,7 +110,8 @@ if __name__ == '__main__':
           f.write(linesep.join([
               'metrics.reporters: file',
               'metrics.reporter.file.class: %s' % file_reporter,
-              'metrics.reporter.file.path: %s' % cls.test_metrics_path
+              'metrics.reporter.file.path: %s' % cls.test_metrics_path,
+              'metrics.scope.operator: <operator_name>',
           ]))
 
     @classmethod
@@ -140,7 +145,7 @@ if __name__ == '__main__':
       options = super(FlinkRunnerTest, self).create_options()
       options.view_as(DebugOptions).experiments = [
           'beam_fn_api'] + extra_experiments
-      options._all_options['parallelism'] = 1
+      options._all_options['parallelism'] = 2
       options._all_options['shutdown_sources_on_final_watermark'] = True
       options.view_as(PortableOptions).environment_type = (
           environment_type.upper())
@@ -218,43 +223,72 @@ if __name__ == '__main__':
           with_transcoding=False)
 
     def test_metrics(self):
-      """Run a simple DoFn that increments a counter, and verify that its
-       expected value is written to a temporary file by the FileReporter"""
+      """Run a simple DoFn that increments a counter and verifies state
+      caching metrics. Verifies that its expected value is written to a
+      temporary file by the FileReporter"""
 
       counter_name = 'elem_counter'
+      state_spec = userstate.BagStateSpec('state', VarIntCoder())
 
       class DoFn(beam.DoFn):
         def __init__(self):
           self.counter = Metrics.counter(self.__class__, counter_name)
           logging.info('counter: %s' % self.counter.metric_name)
 
-        def process(self, v):
+        def process(self, kv, state=beam.DoFn.StateParam(state_spec)):
+          # Trigger materialization
+          list(state.read())
+          state.add(1)
           self.counter.inc()
 
-      p = self.create_pipeline()
-      n = 100
+      options = self.create_options()
+      # Test only supports parallelism of 1
+      options._all_options['parallelism'] = 1
+      # Create multiple bundles to test cache metrics
+      options._all_options['max_bundle_size'] = 10
+      options._all_options['max_bundle_time_millis'] = 95130590130
+      experiments = options.view_as(DebugOptions).experiments or []
+      experiments.append('state_cache_size=123')
+      options.view_as(DebugOptions).experiments = experiments
+      with Pipeline(self.get_runner(), options) as p:
+        # pylint: disable=expression-not-assigned
+        (p
+         | "source" >> FlinkStreamingImpulseSource()
+         .set_interval_ms(0).set_message_count(110)
+         | "mapper" >> beam.Map(lambda x: (int(x.decode('utf-8')) % 10, 'val'))
+         | "stateful" >> beam.ParDo(DoFn()))
 
-      # pylint: disable=expression-not-assigned
-      p \
-      | beam.Create(list(range(n))) \
-      | beam.ParDo(DoFn())
-
-      result = p.run()
-      result.wait_until_finish()
-
+      lines_expected = {'counter: 110',
+                        # Gauges for the last finished bundle
+                        'stateful.beam.metric:statecache:capacity: 123',
+                        'stateful.beam.metric:statecache:size: 10',
+                        'stateful.beam.metric:statecache:get: 10',
+                        'stateful.beam.metric:statecache:miss: 10',
+                        'stateful.beam.metric:statecache:hit: 10',
+                        'stateful.beam.metric:statecache:put: 10',
+                        'stateful.beam.metric:statecache:extend: 10',
+                        'stateful.beam.metric:statecache:evict: 0',
+                        # Counters
+                        # (total of get/hit will be off by 10 due to the caching
+                        # only getting initialized after the first bundle.
+                        # Caching depends on the cache token which is lazily
+                        # initialized by the Runner's StateRequestHandlers).
+                        'stateful.beam.metric:statecache:get_total: 100',
+                        'stateful.beam.metric:statecache:miss_total: 10',
+                        'stateful.beam.metric:statecache:hit_total: 90',
+                        'stateful.beam.metric:statecache:put_total: 10',
+                        'stateful.beam.metric:statecache:extend_total: 100',
+                        'stateful.beam.metric:statecache:evict_total: 0',
+                       }
+      lines_actual = set()
       with open(self.test_metrics_path, 'r') as f:
-        lines = [line for line in f.readlines() if counter_name in line]
-        self.assertEqual(
-            len(lines), 1,
-            msg='Expected 1 line matching "%s":\n%s' % (
-                counter_name, '\n'.join(lines))
-        )
-        line = lines[0]
-        self.assertTrue(
-            '%s: 100' % counter_name in line,
-            msg='Failed to find expected counter %s in line %s' % (
-                counter_name, line)
-        )
+        line = f.readline()
+        while line:
+          for metric_str in lines_expected:
+            if metric_str in line:
+              lines_actual.add(metric_str)
+          line = f.readline()
+      self.assertSetEqual(lines_actual, lines_expected)
 
     def test_sdf_with_sdf_initiated_checkpointing(self):
       raise unittest.SkipTest("BEAM-2939")
