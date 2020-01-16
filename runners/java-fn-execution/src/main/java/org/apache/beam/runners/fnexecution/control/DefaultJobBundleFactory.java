d3ed08a124622c559e4832e170836635fc703b4c
Maximilian Michels <mxm@apache.org>
2020-01-16 13:38:15 +0100

    [BEAM-9132] Avoid logging misleading error messages during pipeline failure
    We have observed these errors in a state-intense application:
    
    ```
    Error processing instruction 107. Original traceback is
    Traceback (most recent call last):
      File "apache_beam/runners/common.py", line 780, in apache_beam.runners.common.DoFnRunner.process
      File "apache_beam/runners/common.py", line 587, in apache_beam.runners.common.PerWindowInvoker.invoke_process
      File "apache_beam/runners/common.py", line 659, in apache_beam.runners.common.PerWindowInvoker._invoke_process_per_window
      File "apache_beam/runners/common.py", line 880, in apache_beam.runners.common._OutputProcessor.process_outputs
      File "apache_beam/runners/common.py", line 895, in apache_beam.runners.common._OutputProcessor.process_outputs
      File "redacted.py", line 56, in process
        recent_events_map = load_recent_events_map(recent_events_state)
      File "redacted.py", line 128, in _load_recent_events_map
        items_in_recent_events_bag = list(recent_events_state.read())
      File "apache_beam/runners/worker/bundle_processor.py", line 335, in __iter__
        for elem in self.first:
      File "apache_beam/runners/worker/bundle_processor.py", line 214, in __iter__
        self._state_key, self._coder_impl, is_cached=self._is_cached)
      File "apache_beam/runners/worker/sdk_worker.py", line 692, in blocking_get
        self._materialize_iter(state_key, coder))
      File "apache_beam/runners/worker/sdk_worker.py", line 723, in _materialize_iter
        self._underlying.get_raw(state_key, continuation_token)
      File "apache_beam/runners/worker/sdk_worker.py", line 603, in get_raw
        continuation_token=continuation_token)))
      File "apache_beam/runners/worker/sdk_worker.py", line 637, in _blocking_request
        raise RuntimeError(response.error)
    RuntimeError: Unknown process bundle instruction id '107'
    ```
    
    Notice that the error is thrown on the Runner side in the GrpcStateService.
    
    Explanation
    ===========
    
    We experienced a network split of the Flink cluster which caused the job to
    fail. The logging implied a failure of the state requests right before the
    network split. However, the opposite is true, the failure occured during
    shutdown of the pipeline where the state request handler has already been removed.
    
    Solution
    ========
    
    Ensure that we shutdown the environment and any pending clients which may be
    processing pending requests.

--- runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/control/DefaultJobBundleFactory.java
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/control/DefaultJobBundleFactory.java
@@ -243,6 +243,14 @@ public class DefaultJobBundleFactory implements JobBundleFactory {
     // Clear the cache. This closes all active environments.
     // note this may cause open calls to be cancelled by the peer
     for (LoadingCache<Environment, WrappedSdkHarnessClient> environmentCache : environmentCaches) {
+      for (WrappedSdkHarnessClient client : environmentCache.asMap().values()) {
+        try {
+          client.close();
+        } catch (Exception e) {
+          // This shouldn't throw because the client catches errors, but just in case...
+          LOG.warn("Error closing client.", e);
+        }
+      }
       environmentCache.invalidateAll();
       environmentCache.cleanUp();
     }
@@ -270,10 +278,11 @@ public class DefaultJobBundleFactory implements JobBundleFactory {
     return outputReceivers;
   }
 
-  private static class PreparedClient {
+  @VisibleForTesting
+  static class PreparedClient {
     private BundleProcessor processor;
     private ExecutableProcessBundleDescriptor processBundleDescriptor;
-    private WrappedSdkHarnessClient wrappedClient;
+    @VisibleForTesting WrappedSdkHarnessClient wrappedClient;
   }
 
   private PreparedClient prepare(
@@ -305,12 +314,13 @@ public class DefaultJobBundleFactory implements JobBundleFactory {
    * A {@link StageBundleFactory} for remotely processing bundles that supports environment
    * expiration.
    */
-  private class SimpleStageBundleFactory implements StageBundleFactory {
+  @VisibleForTesting
+  class SimpleStageBundleFactory implements StageBundleFactory {
 
     private final ExecutableStage executableStage;
     private final int environmentIndex;
     private final HashMap<WrappedSdkHarnessClient, PreparedClient> preparedClients = new HashMap();
-    private PreparedClient currentClient;
+    @VisibleForTesting PreparedClient currentClient;
 
     private SimpleStageBundleFactory(ExecutableStage executableStage) {
       this.executableStage = executableStage;
@@ -445,6 +455,8 @@ public class DefaultJobBundleFactory implements JobBundleFactory {
     }
 
     public void close() {
+      // Invalidate bundle ref count to avoid cleaning up again
+      bundleRefCount.set(-1);
       // DO NOT ADD ANYTHING HERE WHICH MIGHT CAUSE THE BLOCK BELOW TO NOT BE EXECUTED.
       // If we exit prematurely (e.g. due to an exception), resources won't be cleaned up properly.
       // Please make an AutoCloseable and add it to the try statement below.
@@ -465,7 +477,8 @@ public class DefaultJobBundleFactory implements JobBundleFactory {
       // TODO: Wait for executor shutdown?
     }
 
-    private int ref() {
+    @VisibleForTesting
+    int ref() {
       return bundleRefCount.incrementAndGet();
     }
 
