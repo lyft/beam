660e60f9144a85b0ff87b36fd93f3186c8ac09ba
Maximilian Michels <mxm@apache.org>
2020-01-23 20:05:47 +0100

    [BEAM-9161] Ensure non-volatile access of field variables by processing threads
    In the DoFnOperator and the subclassed ExecutableStageDoFnOperator there are
    effectively two processing threads:
    
    (1) The main processing thread for processing elements and watermarks
    (2) A timer thread to support ending bundles after a timeout to optimize for
    latency
    
    Although the code was written with a different assumption in the past, the two
    threads do not interleave with each other. Only one is active at a time. This is
    ensured by Flink's "checkpointLock" which is acquired for every method called on
    the operator like processElement, processWatermark, snapshotState. It is also
    acquired when timers are fired which are set using Flink's TimeService, like it
    is the case for (2).
    
    We've seen issues with bundles being closed multiple times resulting in
    exceptions like "Already closed". Very rarely, we've also seen dead bundle ids
    resurecting for which no other explanation could be found than an inconsistent
    view of the two thread.
    
    This is why this PR makes the fields volatile which are accessed by both
    threads. We do not have to synchronize as explained above. We remove the
    AtomicBool for the bundle state because it is not necessary, a volatile bool
    suffices.

--- runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/translation/wrappers/streaming/ExecutableStageDoFnOperator.java
@@ -130,7 +130,9 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
   private transient StageBundleFactory stageBundleFactory;
   private transient ExecutableStage executableStage;
   private transient SdkHarnessDoFnRunner<InputT, OutputT> sdkHarnessRunner;
-  private transient long backupWatermarkHold = Long.MIN_VALUE;
+
+  /** Watermark held back due to async processing. Volatile due to multiple threads. */
+  private transient volatile long backupWatermarkHold = Long.MIN_VALUE;
 
   /** Constructor. */
   public ExecutableStageDoFnOperator(
@@ -590,8 +592,10 @@ public class ExecutableStageDoFnOperator<InputT, OutputT> extends DoFnOperator<I
     private final BiConsumer<WindowedValue<InputT>, TimerInternals.TimerData> timerRegistration;
     private final Supplier<Object> keyForTimer;
 
-    private RemoteBundle remoteBundle;
-    private FnDataReceiver<WindowedValue<?>> mainInputReceiver;
+    /** Current active bundle. Volatile to ensure bundle threads see this consistent. */
+    private volatile RemoteBundle remoteBundle;
+    /** Current main input receiver. Volatile to ensure bundle threads see this consistent. */
+    private volatile FnDataReceiver<WindowedValue<?>> mainInputReceiver;
 
     public SdkHarnessDoFnRunner(
         String mainInput,
