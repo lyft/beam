7f1b60c3b8faca3d66f155c6b97da1dd4920a38b
Maximilian Michels <mmichels@lyft.com>
2020-01-29 15:15:54 +0000

    Commit for release 2.18.0-lyft20200129bundlesync90

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -297,7 +297,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.18.0'
+    project.version = '2.18.0-lyft20200129bundlesync90'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
