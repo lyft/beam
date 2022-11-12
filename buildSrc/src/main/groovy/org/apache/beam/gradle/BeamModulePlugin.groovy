46eab5c7e240e7cbc6518e79e930371405961ce8
ravimagham <ravimagham@lyft.com>
2022-11-11 21:37:27 -0800

    Revert "Commit for release 2.30.0-lyft202211111668230256-dev"
    This reverts commit cc332ec081a079addbd8b9abfe7cf1795f661f92.

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -361,7 +361,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.30.0-lyft202211111668230256-dev'
+    project.version = '2.30.0'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
