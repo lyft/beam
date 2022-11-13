549c849f9a3f9287fe97b2b359f2525df9810349
ravimagham <ravimagham@lyft.com>
2022-11-12 16:18:20 -0800

    Commit for release 2.30.0-lyft202211121668298700-dev

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -361,7 +361,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.30.0'
+    project.version = '2.30.0-lyft202211121668298700-dev'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
