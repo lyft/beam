541775c351c3b5d691245829dd5b4d14711b3289
Seth Saperstein <sethsaperstein@lyft.com>
2023-12-07 10:44:52 -0800

    Commit for release 2.30.0-lyft202312071701974692

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -361,7 +361,7 @@ class BeamModulePlugin implements Plugin<Project> {
 
     // Automatically use the official release version if we are performing a release
     // otherwise append '-SNAPSHOT'
-    project.version = '2.30.0'
+    project.version = '2.30.0-lyft202312071701974692'
     if (!isRelease(project)) {
       project.version += '-SNAPSHOT'
     }
